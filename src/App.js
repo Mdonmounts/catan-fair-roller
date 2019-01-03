d0ff4534412c9924fc9521dc63164655d9b7c62a
Mitchell Mounts <mmounts@salesforce.com>
2019-01-03 11:56:36 -0500

    Create 'fair' roller with a graph to demonstrate bell curve.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import NormalizedRoller from './utilities/normal-roller';
+import Plot from 'react-plotly.js';
 import './App.css';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            currentRoll: 0,
+            outcomes: []
+        };
+        this.roller = new NormalizedRoller();
+    }
+
+    rollDice = () => {
+        const newRoll = this.roller.getNextRoll();
+        this.setState({
+            currentRoll: newRoll,
+            outcomes: this.roller.getObservations()
+        });
+    }
+
+    simulate = () => {
+        for(let i = 0; i < 60; i++) {
+            this.roller.getNextRoll();
+        }
+        this.rollDice();
+    }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1>{this.state.currentRoll}</h1>
+          <button onClick={this.rollDice}>Roll!</button>
+          <button onClick={this.simulate}>Simulate a game!</button>
+          <Plot
+            data={[{type: 'bar', x:[2,3,4,5,6,7,8,9,10,11,12], y:this.state.outcomes}]}
+            layout={{width: 800, height: 800, title: 'Rolls!'}}
+          />
         </header>
       </div>
     );
